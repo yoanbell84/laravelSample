dc4b089b75cf3d24e23b9396a68ce530143b7126
Yoan <yoanbell84@gmail.com>
2018-10-15 18:22:57 -0400

    Keeping styles

--- webpack.mix.js
+++ webpack.mix.js
@@ -8,8 +8,20 @@ let mix = require('laravel-mix');
  | Mix provides a clean, fluent API for defining some Webpack build steps
  | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
- |
+ |  dd
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.sass('resources/assets/sass/app.scss', 'public/css');
+
+mix.js(['resources/assets/js/app.js','resources/assets/js/libs/metisMenu.js',
+'resources/assets/js/libs/sb-admin-2.js',
+'resources/assets/js/libs/scripts.js'],
+'public/js/app.js');
+
+mix.styles([
+    'resources/assets/sass/libs/bootstrap.css',
+    'resources/assets/sass/libs/font-awesome.css',
+    'resources/assets/sass/libs/metisMenu.css',
+    'resources/assets/sass/libs/sb-admin-2.css',
+    'resources/assets/sass/libs/styles.css'
+], 'public/css/libs.css'); 
