622fb9b80973a3523570632bcf8229115fdd61ed
yoabell <yoabell84@gmail.com>
2018-10-15 20:56:31 -0400

    Latest

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use App\Role;
+use App\Http\Requests\UsersRequest;
 
 class AdminUsersController extends Controller
 {
@@ -13,8 +16,9 @@ class AdminUsersController extends Controller
      */
     public function index()
     {
-        //
-        return view('admin.users.index');
+        
+        $users = User::all();
+        return view('admin.users.index' , compact('users'));
     }
 
     /**
@@ -25,7 +29,8 @@ class AdminUsersController extends Controller
     public function create()
     {
         //
-         return view('admin.users.create');
+         $roles = Role::pluck('name','id');
+         return view('admin.users.create' , compact('roles'));
     }
 
     /**
@@ -34,9 +39,11 @@ class AdminUsersController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UsersRequest $request)
     {
         //
+        
+        dd($request);
     }
 
     /**
--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -1,7 +1,60 @@
-<?php
-/* 
-* To change this license header, choose License Headers in Project Properties.
-* To change this template file, choose Tools | Templates
-* and open the template in the editor.
-*/
+@extends('layouts.admin')
+
+@section('content')
+
+<h1> Create Users</h1>
+ 
+{!! Form::open(['method'=>'POST','action' => 'AdminUsersController@store' , 'files' => true]) !!} 
+   
+   <div class="form-group">
+       {!!Form::Label('name','Name')!!}
+       {!!Form::text('name',null,['class'=>'form-control'])!!}
+        @if ($errors->has('name'))
+        <span class="text-danger">{{ $errors->first('name') }}</span>
+        @endif
+   </div>
+    <div class="form-group">
+       {!!Form::Label('email','Email')!!}
+       {!!Form::text('email',null,['class'=>'form-control'])!!}
+         @if ($errors->has('email'))
+        <span class="text-danger">{{ $errors->first('email') }}</span>
+        @endif
+    </div>
+    <div class="form-group">
+        {!!Form::Label('role_id','Role')!!}
+        {!!Form::select('role_id', $roles , 3, ['class'=>'form-control'])!!}
+      
+    </div>
+    <div class="form-group">
+        {!!Form::Label('status','Status')!!}
+        {!!Form::select('status', ['1' => 'Active', '0' => 'Not Active'], 1, ['class'=>'form-control'])!!}
+    </div>
+    <div class="form-group">
+       {!!Form::Label('password','Password')!!}
+       {!!Form::password('password',['class'=>'form-control'])!!}
+         @if ($errors->has('password'))
+        <span class="text-danger">{{ $errors->first('password') }}</span>
+        @endif
+   </div>
+    <div class="form-group">
+       {!!Form::Label('confirmPassword','Confirm Password')!!}
+       {!!Form::password('confirmPassword',['class'=>'form-control'])!!}
+         @if ($errors->has('confirmPassword'))
+        <span class="text-danger">{{ $errors->first('confirmPassword') }}</span>
+        @endif
+   </div>
+    <div class="form-group">
+         {!!Form::Label('avatar','Avatar')!!}
+      {!!Form::file('avatar',['class'=>'form-control'])!!}
+    </div>
+<br>
+   <div class="form-group">
+        {!!Form::submit('Create User',['class'=>'btn btn-primary'])!!}
+   </div>
+
+    {!! Form::close() !!}
+
+    @include('admin.includes.formErrors')
+    
+@stop
 
