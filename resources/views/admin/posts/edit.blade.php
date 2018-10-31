8906ed25f33fade1c250fbde8e40b2664dd34051
yoabell <yoabell84@gmail.com>
2018-10-31 19:32:00 -0400

    Creating Post with categories changes

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -23,7 +23,7 @@
    </div>
     <div class="form-group">
        {!!Form::Label('category_id','Category')!!}
-      {!!Form::select('category_id', $categories , null , ['class'=>'form-control'])!!}
+      {!!Form::select('category_id', $categories , null , ['class'=>'form-control' , 'placeholder'=>'Chose a category'])!!}
          @if ($errors->has('category_id'))
         <span class="text-danger">{{ $errors->first('category') }}</span>
         @endif
